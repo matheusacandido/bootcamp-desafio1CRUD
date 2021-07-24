c5a43c3a90d236fb437c30e4e4a3d045d05a3404
matheusacandido <matheusacandido@hotmail.com>
2021-07-23 21:42:56 -0300

    Backend project created

---
+++ backend/src/main/java/com/candmat/desafio1CRUD/Desafio1CrudApplication.java
@@ -0,0 +1,13 @@
+package com.candmat.desafio1CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio1CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio1CrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/candmat/desafio1CRUD/Desafio1CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candmat.desafio1CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio1CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
