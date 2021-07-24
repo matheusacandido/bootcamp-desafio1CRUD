8ff915b13644190d65cf1808cec725f58a9a955a
matheusacandido <matheusacandido@hotmail.com>
2021-07-24 00:52:25 -0300

    Post and Put OK

--- backend/src/main/java/com/candmat/desafio1CRUD/resources/ClientResource.java
+++ backend/src/main/java/com/candmat/desafio1CRUD/resources/ClientResource.java
@@ -1,5 +1,7 @@
 package com.candmat.desafio1CRUD.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -7,12 +9,17 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.candmat.desafio1CRUD.dto.ClientDTO;
 import com.candmat.desafio1CRUD.services.ClientService;
+
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -22,9 +29,10 @@ public class ClientResource {
 	private ClientService service;
 
 	@GetMapping
-	public ResponseEntity<Page<ClientDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "DESC") String direction,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
 			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
 
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
@@ -40,5 +48,18 @@ public class ClientResource {
 		return ResponseEntity.ok().body(dto);
 	}
 
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insertClient(@RequestBody ClientDTO dto) {
+		dto = service.insertClient(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> updateClient(@PathVariable Long id, @RequestBody ClientDTO dto) {
+		dto = service.updateClient(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
 	
 }
--- backend/src/main/java/com/candmat/desafio1CRUD/services/ClientService.java
+++ backend/src/main/java/com/candmat/desafio1CRUD/services/ClientService.java
@@ -2,6 +2,8 @@ package com.candmat.desafio1CRUD.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -23,7 +25,7 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> listClient = repository.findAll(pageRequest);
 		return listClient.map(k -> new ClientDTO(k));
-}
+	}
 
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
@@ -32,6 +34,34 @@ public class ClientService {
 		return new ClientDTO(entity);
 	}
 
+	@Transactional
+	public ClientDTO insertClient(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
 
+	@Transactional
+	public ClientDTO updateClient(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("This id(" + id + ") was not found");
+		}
+	}
+
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+
+	}
 
 }
